fe64d1cda67c15ddfe40583adc2f812f3a37e8f5
Alexis Perrier <alexis.perrier@gmail.com>
2024-02-25 11:41:48 +0100

    format

--- streamlitapp/google_storage.py
+++ streamlitapp/google_storage.py
@@ -1,5 +1,5 @@
-'''
-'''
+"""
+"""
 
 import os
 import json
@@ -9,29 +9,25 @@ from google.cloud import storage
 
 
 class StorageWrap(object):
-
     @classmethod
     def get_credentials_from_base64(cls) -> service_account.Credentials:
-
         encoded_credentials = os.environ.get("GOOGLE_CLOUD_CREDENTIALS_BASE64")
         if encoded_credentials is None:
             raise ValueError("The GOOGLE_CLOUD_CREDENTIALS_BASE64 environment variable is not set.")
         credentials_json = base64.b64decode(encoded_credentials).decode("utf-8")
         return service_account.Credentials.from_service_account_info(json.loads(credentials_json))
 
-    def __init__(self ) -> None:
-        self.client = storage.Client(
-            credentials=StorageWrap.get_credentials_from_base64()
-        )
-        assert self.client.project == 'skatai', "Storage Client can't access project name"
+    def __init__(self) -> None:
+        self.client = storage.Client(credentials=StorageWrap.get_credentials_from_base64())
+        assert self.client.project == "skatai", "Storage Client can't access project name"
 
-    def set_bucket(self, bucket_name:str = 'ragtime-ai-act') -> None:
+    def set_bucket(self, bucket_name: str = "ragtime-ai-act") -> None:
         self.bucket_name = bucket_name
         self.bucket = self.client.get_bucket(self.bucket_name)
 
-    def save_json(self,json_data,filename):
+    def save_json(self, json_data, filename):
         blob = self.bucket.blob(filename)
-        blob.upload_from_string(json_data, content_type='application/json')
+        blob.upload_from_string(json_data, content_type="application/json")
 
     def file_info(self, filename):
         self.bucket = self.client.get_bucket(self.bucket_name)
@@ -48,10 +44,11 @@ class StorageWrap(object):
     def list_blobs(self):
         return [blob.name for blob in self.bucket.list_blobs()]
 
+
 if __name__ == "__main__":
     sw = StorageWrap()
     buckets = sw.list_buckets()
     print(buckets)
-    sw.set_bucket('ragtime-ai-act')
+    sw.set_bucket("ragtime-ai-act")
     blobs = sw.list_blobs()
     print(blobs)
--- streamlitapp/retrieve.py
+++ streamlitapp/retrieve.py
@@ -4,6 +4,7 @@ import numpy as np
 import typing as t
 import tempfile
 import datetime
+
 # streamlit
 import streamlit as st
 
@@ -24,13 +25,17 @@ from langchain.chains import SequentialChain
 from streamlit_weaviate_utils import *
 from google_storage import StorageWrap
 
-class Retrieve(object):
 
+class Retrieve(object):
     # def __init__(self, query, search_type, model, number_elements, temperature, author):
     def __init__(self, query, search_params):
         # collection_name = "AIActKnowledgeBase"
-        self.authors = {'all versions': None,
-            '2024 coreper':'coreper', '2022 council':'council', '2021 commission': 'commission'}
+        self.authors = {
+            "all versions": None,
+            "2024 coreper": "coreper",
+            "2022 council": "council",
+            "2021 commission": "commission",
+        }
         collection_name = "AIAct_240220"
         cluster_location = "cloud"
         self.client = connect_client(cluster_location)
@@ -42,15 +47,15 @@ class Retrieve(object):
         self.collection = self.client.collections.get(collection_name)
         # TODO safeguard query
         self.query = query
-        self.author = self.authors.get(search_params.get('author'))
-        self.model = search_params.get('model')
-        self.search_type = search_params.get('search_type')
-        self.response_count_ = search_params.get('number_elements')
-        self.temperature = search_params.get('temperature')
+        self.author = self.authors.get(search_params.get("author"))
+        self.model = search_params.get("model")
+        self.search_type = search_params.get("search_type")
+        self.response_count_ = search_params.get("number_elements")
+        self.temperature = search_params.get("temperature")
         # generative
 
-        self.prompt_generative_context =ChatPromptTemplate.from_template(
-'''You are a journalist from Euractiv who is an expert on both Artifical Intelligence, the AI-act regulation from the European Union and European policy making.
+        self.prompt_generative_context = ChatPromptTemplate.from_template(
+            """You are a journalist from Euractiv who is an expert on both Artifical Intelligence, the AI-act regulation from the European Union and European policy making.
 Your goal is to make it easier for people to understand the AI-Act from the UE.
 
 You are given a query and context information.
@@ -65,10 +70,11 @@ Your specific task is to answer the query based on the context information.
 --- Context:
 {context}
 --- Query:
-{query}''')
+{query}"""
+        )
 
         self.prompt_generative_bare = ChatPromptTemplate.from_template(
-'''You are a journalist from Euractiv who is an expert on both Artifical Intelligence, the AI-act regulation from the European Union and European policy making.
+            """You are a journalist from Euractiv who is an expert on both Artifical Intelligence, the AI-act regulation from the European Union and European policy making.
 Your goal is to make it easier for people to understand the AI-Act from the UE.
 
 # make sure to:
@@ -78,13 +84,19 @@ Your goal is to make it easier for people to understand the AI-Act from the UE.
 - Do not try to imagine a fake answer if you don't have the necessary information
 
 --- Query:
-{query}''')
+{query}"""
+        )
 
-        self.answer_with_context = ''
-        self.answer_bare = ''
+        self.answer_with_context = ""
+        self.answer_bare = ""
 
         self.llm = ChatOpenAI(temperature=self.temperature, model=self.model)
-        self.context_chain   = LLMChain(llm=self.llm, prompt=self.prompt_generative_context,  output_key="answer_context", verbose=False)
+        self.context_chain = LLMChain(
+            llm=self.llm,
+            prompt=self.prompt_generative_context,
+            output_key="answer_context",
+            verbose=False,
+        )
         self.chunk_uuids = []
         self.chunk_titles = []
         self.chunk_texts = []
@@ -93,20 +105,25 @@ Your goal is to make it easier for people to understand the AI-Act from the UE.
             chains=[self.context_chain],
             input_variables=["context", "query"],
             output_variables=["answer_context"],
-            verbose=True
+            verbose=True,
         )
 
-        self.bare_chain   = LLMChain(llm=self.llm, prompt=self.prompt_generative_bare,  output_key="answer_bare", verbose=False)
+        self.bare_chain = LLMChain(
+            llm=self.llm,
+            prompt=self.prompt_generative_bare,
+            output_key="answer_bare",
+            verbose=False,
+        )
 
         self.overall_bare_chain = SequentialChain(
             chains=[self.bare_chain],
             input_variables=["query"],
             output_variables=["answer_bare"],
-            verbose=True
+            verbose=True,
         )
 
     def search(self):
-        filters = Filter("content_type").not_equal('header')
+        filters = Filter("content_type").not_equal("header")
         # filters = None
         if self.author is not None:
             if filters is None:
@@ -116,37 +133,36 @@ Your goal is to make it easier for people to understand the AI-Act from the UE.
 
         if self.search_type == "hybrid":
             self.response = self.collection.query.hybrid(
-                    query=self.query,
-                    query_properties = ['text'],
-                    filters= filters,
-                    limit=self.response_count_,
-                    return_metadata = ['score', 'explain_score', 'is_consistent'],
-                )
+                query=self.query,
+                query_properties=["text"],
+                filters=filters,
+                limit=self.response_count_,
+                return_metadata=["score", "explain_score", "is_consistent"],
+            )
         elif self.search_type == "near_text":
             self.response = self.collection.query.near_text(
-                    query= self.query,
-                    filters= filters,
-                    limit=self.response_count_,
-                    return_metadata = ['distance','certainty'],
-                )
+                query=self.query,
+                filters=filters,
+                limit=self.response_count_,
+                return_metadata=["distance", "certainty"],
+            )
         self.get_context()
 
     def generate_answer_with_context(self):
-        self.response_context = self.overall_context_chain({ "context": self.context, "query": self.query })
-        self.answer_with_context = self.response_context['answer_context']
+        self.response_context = self.overall_context_chain({"context": self.context, "query": self.query})
+        self.answer_with_context = self.response_context["answer_context"]
 
     def generate_answer_bare(self):
-        self.response_bare = self.overall_bare_chain({ "query": self.query })
-        self.answer_bare = self.response_bare['answer_bare']
+        self.response_bare = self.overall_bare_chain({"query": self.query})
+        self.answer_bare = self.response_bare["answer_bare"]
 
     def get_context(self):
         texts = []
         self.chunk_uuids = []
         self.chunk_titles = []
         for i in range(self.response_count_):
-
             prop = self.response.objects[i].properties
-            self.chunk_uuids.append(prop.get('uuid'))
+            self.chunk_uuids.append(prop.get("uuid"))
             text = "---"
             # if prop.get('section') == 'recitals':
             #     title = prop.get('rec')
@@ -154,19 +170,19 @@ Your goal is to make it easier for people to understand the AI-Act from the UE.
             #     title = [prop.get(lct) for lct in ['ttl', 'art', 'par'] if prop.get(lct) is not None ]
             #     title = " >> ".join(title)
 
-            text += ' - '.join([prop.get('title'), prop.get('author') ])
+            text += " - ".join([prop.get("title"), prop.get("author")])
             text += "\n"
-            text += prop.get('text')
-            self.chunk_titles.append(' - '.join([prop.get('title'), prop.get('author') ]))
+            text += prop.get("text")
+            self.chunk_titles.append(" - ".join([prop.get("title"), prop.get("author")]))
             texts.append(text)
-        self.context = '\n'.join(texts)
+        self.context = "\n".join(texts)
         self.chunk_texts = texts
 
     # format
     def format_metadata(self, i):
-        metadata_str =[]
+        metadata_str = []
         if self.search_type == "hybrid":
-            metadata_str =f"**score**: {np.round(self.response.objects[i].metadata.score, 4)} "
+            metadata_str = f"**score**: {np.round(self.response.objects[i].metadata.score, 4)} "
         elif self.search_type == "near_text":
             metadata_str = f"distance: {np.round(self.response.objects[i].metadata.distance, 4)} certainty: {np.round(self.response.objects[i].metadata.certainty, 4)} "
         st.write(metadata_str)
@@ -180,52 +196,52 @@ Your goal is to make it easier for people to understand the AI-Act from the UE.
         # if prop.get('section') == 'articles':
         #     location = [prop.get(lct) for lct in ['ttl', 'art', 'par'] if prop.get(lct) is not None ]
         #     location = " >> ".join(location)
-        title = ' - '.join([prop.get('title'), prop.get('author') ])
-        return  f"**{title}**"
+        title = " - ".join([prop.get("title"), prop.get("author")])
+        return f"**{title}**"
 
     def format_properties(self, i):
         prop = self.response.objects[i].properties
-        st.write(prop['text'].strip())
+        st.write(prop["text"].strip())
 
     def log_session(self):
-        os.write(1,bytes("--"*20 + "\n", 'utf-8'))
-        os.write(1,bytes(f"query: {self.query}\n" , 'utf-8'))
-        os.write(1,bytes(f"search_type: {self.search_type}\n" , 'utf-8'))
-        os.write(1,bytes(f"model: {self.model}\n" , 'utf-8'))
-        os.write(1,bytes(f"response_count_: {self.response_count_}\n" , 'utf-8'))
-        os.write(1,bytes(f"temperature: {self.temperature}\n" , 'utf-8'))
-        os.write(1,bytes(f"author: {self.author}\n" , 'utf-8'))
-        os.write(1,bytes(f"answer with context:\n {self.answer_with_context}\n" , 'utf-8'))
-        os.write(1,bytes("--"*20 + "\n\n", 'utf-8'))
+        os.write(1, bytes("--" * 20 + "\n", "utf-8"))
+        os.write(1, bytes(f"query: {self.query}\n", "utf-8"))
+        os.write(1, bytes(f"search_type: {self.search_type}\n", "utf-8"))
+        os.write(1, bytes(f"model: {self.model}\n", "utf-8"))
+        os.write(1, bytes(f"response_count_: {self.response_count_}\n", "utf-8"))
+        os.write(1, bytes(f"temperature: {self.temperature}\n", "utf-8"))
+        os.write(1, bytes(f"author: {self.author}\n", "utf-8"))
+        os.write(1, bytes(f"answer with context:\n {self.answer_with_context}\n", "utf-8"))
+        os.write(1, bytes("--" * 20 + "\n\n", "utf-8"))
         pass
 
     def to_dict(self) -> t.Dict:
         return {
-            'date': datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
-            'query': self.query,
-            'author': self.author,
-            'search': {
-                'search_type': self.search_type,
-                'model': self.model,
-                'response_count_': self.response_count_,
-                'temperature': self.temperature,
+            "date": datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "query": self.query,
+            "author": self.author,
+            "search": {
+                "search_type": self.search_type,
+                "model": self.model,
+                "response_count_": self.response_count_,
+                "temperature": self.temperature,
             },
-            'answer': {
-                'answer_with_context': self.answer_with_context,
-                'answer_bare': self.answer_bare,
+            "answer": {
+                "answer_with_context": self.answer_with_context,
+                "answer_bare": self.answer_bare,
             },
-            'context': {
-                'uuids': ','.join([str(uuid) for uuid in self.chunk_uuids]),
-                'titles': ','.join([str(uuid) for uuid in self.chunk_titles]),
-                'texts': self.chunk_texts,
+            "context": {
+                "uuids": ",".join([str(uuid) for uuid in self.chunk_uuids]),
+                "titles": ",".join([str(uuid) for uuid in self.chunk_titles]),
+                "texts": self.chunk_texts,
             },
         }
 
     def to_bucket(self):
         sw = StorageWrap()
-        sw.set_bucket('ragtime-ai-act')
+        sw.set_bucket("ragtime-ai-act")
         json_data = self.to_dict()
-        with tempfile.NamedTemporaryFile(delete=False, mode='w', suffix='.json') as temp_file:
+        with tempfile.NamedTemporaryFile(delete=False, mode="w", suffix=".json") as temp_file:
             json.dump(json_data, temp_file)
             temp_file.flush()
             # upload
@@ -238,7 +254,6 @@ Your goal is to make it easier for people to understand the AI-Act from the UE.
 
 
 if __name__ == "__main__":
-
     import json
     import tempfile
     from google.cloud import storage
@@ -249,13 +264,12 @@ if __name__ == "__main__":
 
     # Create a temporary file
     sw = StorageWrap()
-    sw.set_bucket('ragtime-ai-act')
+    sw.set_bucket("ragtime-ai-act")
 
-    with tempfile.NamedTemporaryFile(delete=False, mode='w', suffix='.json') as temp_file:
+    with tempfile.NamedTemporaryFile(delete=False, mode="w", suffix=".json") as temp_file:
         json.dump(json_data, temp_file)
         temp_file.flush()
         blob = sw.bucket.blob(f"sessions/{temp_file.name.split('/')[-1]}")
         blob.upload_from_filename(temp_file.name)
 
     blobs = sw.list_blobs()
-
